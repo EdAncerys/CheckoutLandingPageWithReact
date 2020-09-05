25bd6afa2de6083676c3edef9725ddfe3b341696
Ed <lookatemail@gmail.com>
2020-09-05 16:43:56 +0100

    Create NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NavBar({ props }) {
+  return (
+    <div style={styles.container}>
+      <div>Checkout</div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {},
+};
