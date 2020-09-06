27700c4d8c9e1d2df9a6eacb5210cc992d7d4821
Ed <lookatemail@gmail.com>
2020-09-06 13:28:02 +0100

    Add main web app colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../src/css/App.css';
 import NavBar from './components/NavBar';
 
 export default function App({ props }) {
---
+++ src/config/colors.jsx
@@ -0,0 +1,10 @@
+export default {
+  primary: '#0B1048',
+  secondary: '#67D1D9',
+  black: '#0e2433',
+  white: '#fff',
+  medium: '#808080',
+  light: '#f8f4f4',
+  dark: '#0c0c0c',
+  danger: '#ff5252',
+};
