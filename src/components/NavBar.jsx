54ced1e19eb3325fa863c4909603d2e442784e4f
Ed <lookatemail@gmail.com>
2020-09-06 13:42:06 +0100

    Add react icons to the project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { FaCodepen } from 'react-icons/fa';
 
 export default function NavBar({ props }) {
   return (
     <div style={styles.container}>
-      <div>Checkout</div>
+      <div style={styles.logo}>
+        <FaCodepen /> Checkout
+      </div>
+      <div style={styles.nav}>Checkout</div>
     </div>
   );
 }
 
 const styles = {
-  container: {},
+  container: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 2fr',
+    alignItems: 'center',
+    textAlign: 'center',
+    width: '100vw',
+    height: 100,
+  },
 };
