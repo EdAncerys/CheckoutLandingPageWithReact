107b2f8c3c0b6249cc95c0ae73e55115b2cefae0
Ed <lookatemail@gmail.com>
2020-09-10 14:32:42 +0100

    Add CustommerStories title to banner

--- src/components/BrandComponent/CustomerStories.jsx
+++ src/components/BrandComponent/CustomerStories.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import colors from '../../config/colors';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 export default function CustomerStories({ props }) {
   return (
     <div style={styles.container}>
-      <div style={styles.heading}>
+      <div style={styles.boxOne}>
         Customer stories
-        <div style={styles.title}>
-          Discover our difference View all case studies
+        <div style={styles.boxTwo}>
+          <div style={styles.boxTwoA}>
+            Discover our difference View all case studies
+          </div>
+          <div style={styles.boxTwoB}>
+            <div>View all case studies</div>
+            <BsArrowRightShort size={25} />
+          </div>
         </div>
       </div>
     </div>
@@ -14,5 +22,28 @@ export default function CustomerStories({ props }) {
 }
 
 const styles = {
-  container: {},
+  container: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    width: 1000,
+    marginBottom: 50,
+  },
+  boxOne: {
+    fontWeight: 'normal',
+  },
+  boxTwo: {
+    display: 'grid',
+    gridTemplateColumns: 'auto auto',
+    fontSize: 35,
+    marginTop: 10,
+    color: colors.primary,
+  },
+  boxTwoA: { width: 'auto' },
+  boxTwoB: {
+    display: 'grid',
+    gridTemplateColumns: 'auto auto',
+    fontSize: 18,
+    alignSelf: 'end',
+    justifyItems: 'end',
+  },
 };
